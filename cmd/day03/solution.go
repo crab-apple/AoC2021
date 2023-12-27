5130a133ab40ef8de9dd74e04c5dc7935bbb901e
crab-apple <andreu.bastardas@gmail.com>
2023-12-28 00:57:38 +0100

    Day 3 part 1

--- cmd/day03/solution.go
+++ cmd/day03/solution.go
@@ -1,7 +1,33 @@
 package day03
 
+import (
+	"github.com/crab-apple/AoC2021/internal/input"
+)
+
 func SolvePart1(s string) int {
-	return 0
+
+	lines := input.ReadLines(s)
+	numBits := len(lines[0])
+	var cnts [64]int
+
+	for _, line := range lines {
+		for i := range line {
+			if line[i] == '1' {
+				cnts[i]++
+			}
+		}
+	}
+
+	var gamma, epsilon int
+	for i := 0; i < numBits; i++ {
+		if cnts[i] > len(lines)/2 {
+			gamma |= (1 << (numBits - 1)) >> i
+		} else {
+			epsilon |= (1 << (numBits - 1)) >> i
+		}
+	}
+
+	return epsilon * gamma
 }
 
 func SolvePart2(s string) int {
--- cmd/day03/solution_test.go
+++ cmd/day03/solution_test.go
@@ -8,15 +8,26 @@ import (
 )
 
 var testInput = `
-Test input here
+00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010
 `
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, day03.SolvePart1(testInput))
+	assert.Equal(t, 198, day03.SolvePart1(testInput))
 }
 
 func TestPart1RealInput(t *testing.T) {
-	assert.Equal(t, 0, day03.SolvePart1(input.ReadInputFile()))
+	assert.Equal(t, 693486, day03.SolvePart1(input.ReadInputFile()))
 }
 
 func TestPart2(t *testing.T) {
