4ee508bd9368fefc13895891322cb1296bb67e46
crab-apple <andreu.bastardas@gmail.com>
2023-12-28 22:54:42 +0100

    Day 5 part 1

--- cmd/day05/solution.go
+++ cmd/day05/solution.go
@@ -1,9 +1,79 @@
 package day05
 
+import (
+	"github.com/crab-apple/AoC2021/internal/input"
+	"github.com/crab-apple/AoC2021/internal/utils"
+	"log"
+	"strings"
+)
+
 func SolvePart1(s string) int {
-	return 0
+	vents := VentSet(utils.Filter(parseInput(s), func(vent Vent) bool {
+		return vent.IsHorizontalOrVertical()
+	}))
+	overlapCount := 0
+	for i := 0; i < 1000; i++ {
+		for j := 0; j < 1000; j++ {
+			coords := Coords{i, j}
+			if vents.ThereIsOverlap(coords) {
+				overlapCount++
+			}
+		}
+	}
+	return overlapCount
 }
 
 func SolvePart2(s string) int {
 	return 0
 }
+
+func parseInput(s string) []Vent {
+	return input.Read(s, parseVent)
+}
+
+func parseVent(s string) Vent {
+	return Vent{
+		parseCoords(strings.Split(s, " -> ")[0]),
+		parseCoords(strings.Split(s, " -> ")[1]),
+	}
+}
+
+func parseCoords(s string) Coords {
+	return Coords{
+		input.ToInt(strings.Split(s, ",")[0]),
+		input.ToInt(strings.Split(s, ",")[1]),
+	}
+}
+
+type VentSet []Vent
+
+func (v VentSet) ThereIsOverlap(c Coords) bool {
+	ventCount := utils.Count(v, func(vent Vent) bool {
+		return vent.Contains(c)
+	})
+	return ventCount > 1
+}
+
+type Vent struct{ Start, End Coords }
+
+func (v Vent) Contains(c Coords) bool {
+	if !v.IsHorizontalOrVertical() {
+		log.Panic("Not implemented")
+	}
+	return contained(v.Start.X, v.End.X, c.X) && contained(v.Start.Y, v.End.Y, c.Y)
+}
+
+func contained(rangeStart int, rangeEnd int, i int) bool {
+	if rangeStart > rangeEnd {
+		temp := rangeEnd
+		rangeEnd = rangeStart
+		rangeStart = temp
+	}
+	return rangeStart <= i && rangeEnd >= i
+}
+
+func (v Vent) IsHorizontalOrVertical() bool {
+	return v.Start.X == v.End.X || v.Start.Y == v.End.Y
+}
+
+type Coords struct{ X, Y int }
--- cmd/day05/solution_test.go
+++ cmd/day05/solution_test.go
@@ -8,15 +8,24 @@ import (
 )
 
 var testInput = `
-Test input here
+0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
 `
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, day05.SolvePart1(testInput))
+	assert.Equal(t, 5, day05.SolvePart1(testInput))
 }
 
 func TestPart1RealInput(t *testing.T) {
-	assert.Equal(t, 0, day05.SolvePart1(input.ReadInputFile()))
+	assert.Equal(t, 5585, day05.SolvePart1(input.ReadInputFile()))
 }
 
 func TestPart2(t *testing.T) {
@@ -25,4 +34,41 @@ func TestPart2(t *testing.T) {
 
 func TestPart2RealInput(t *testing.T) {
 	assert.Equal(t, 0, day05.SolvePart2(input.ReadInputFile()))
+}
+
+func TestVent_Contains(t *testing.T) {
+	vent := day05.Vent{day05.Coords{2, 3}, day05.Coords{20, 3}}
+
+	assert.False(t, vent.Contains(day05.Coords{1, 3}))
+	assert.False(t, vent.Contains(day05.Coords{21, 3}))
+	assert.True(t, vent.Contains(day05.Coords{2, 3}))
+	assert.True(t, vent.Contains(day05.Coords{20, 3}))
+	assert.True(t, vent.Contains(day05.Coords{15, 3}))
+	assert.False(t, vent.Contains(day05.Coords{15, 2}))
+}
+
+func TestVent_ContainsReversed(t *testing.T) {
+	vent := day05.Vent{day05.Coords{20, 3}, day05.Coords{2, 3}}
+
+	assert.False(t, vent.Contains(day05.Coords{1, 3}))
+	assert.False(t, vent.Contains(day05.Coords{21, 3}))
+	assert.True(t, vent.Contains(day05.Coords{2, 3}))
+	assert.True(t, vent.Contains(day05.Coords{20, 3}))
+	assert.True(t, vent.Contains(day05.Coords{15, 3}))
+	assert.False(t, vent.Contains(day05.Coords{15, 2}))
+}
+
+func TestVentSet_ThereIsOverlap(t *testing.T) {
+
+	vs := day05.VentSet{
+		day05.Vent{day05.Coords{2, 3}, day05.Coords{2, 8}},
+		day05.Vent{day05.Coords{2, 6}, day05.Coords{2, 10}},
+	}
+
+	assert.False(t, vs.ThereIsOverlap(day05.Coords{2, 0}))
+	assert.False(t, vs.ThereIsOverlap(day05.Coords{2, 5}))
+	assert.True(t, vs.ThereIsOverlap(day05.Coords{2, 6}))
+	assert.True(t, vs.ThereIsOverlap(day05.Coords{2, 7}))
+	assert.True(t, vs.ThereIsOverlap(day05.Coords{2, 8}))
+	assert.False(t, vs.ThereIsOverlap(day05.Coords{2, 9}))
 }
