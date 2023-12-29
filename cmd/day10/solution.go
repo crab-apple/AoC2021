61c1019c4781871717c76b19bd807b7996109335
crab-apple <andreu.bastardas@gmail.com>
2023-12-30 00:52:15 +0100

    Day 10 part 2

--- cmd/day10/solution.go
+++ cmd/day10/solution.go
@@ -3,24 +3,36 @@ package day10
 import (
 	"github.com/crab-apple/AoC2021/internal/input"
 	"github.com/crab-apple/AoC2021/internal/utils"
+	"sort"
 )
 
-var scoreSheet = map[rune]int{
+var corruptedScoreSheet = map[rune]int{
 	')': 3,
 	']': 57,
 	'}': 1197,
 	'>': 25137,
 }
 
+var incompleteScoreSheet = map[rune]int{
+	')': 1,
+	']': 2,
+	'}': 3,
+	'>': 4,
+}
+
 func SolvePart1(s string) int {
-	return utils.Sum(utils.Map(input.ReadLines(s), score))
+	return utils.Sum(utils.Map(input.ReadLines(s), scoreCorrupted))
 }
 
 func SolvePart2(s string) int {
-	return 0
+	scores := utils.Filter(utils.Map(input.ReadLines(s), scoreIncomplete), func(i int) bool {
+		return i != 0
+	})
+	sort.Ints(scores)
+	return scores[len(scores)/2]
 }
 
-func score(line string) int {
+func scoreCorrupted(line string) int {
 	expected := utils.Stack[rune]{}
 	for _, r := range line {
 		switch r {
@@ -34,9 +46,34 @@ func score(line string) int {
 			expected.Push('>')
 		default:
 			if expected.Pop() != r {
-				return scoreSheet[r]
+				return corruptedScoreSheet[r]
 			}
 		}
 	}
 	return 0
+}
+func scoreIncomplete(line string) int {
+	expected := utils.Stack[rune]{}
+	for _, r := range line {
+		switch r {
+		case '(':
+			expected.Push(')')
+		case '[':
+			expected.Push(']')
+		case '{':
+			expected.Push('}')
+		case '<':
+			expected.Push('>')
+		default:
+			if expected.Pop() != r {
+				return 0
+			}
+		}
+	}
+	score := 0
+	for len(expected) != 0 {
+		score *= 5
+		score += incompleteScoreSheet[expected.Pop()]
+	}
+	return score
 }
