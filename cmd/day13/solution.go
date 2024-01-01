37635abf1df4a5321d969e63938da4841e070a7e
crab-apple <andreu.bastardas@gmail.com>
2024-01-01 10:28:08 +0100

    Day 13 part 1

--- cmd/day13/solution.go
+++ cmd/day13/solution.go
@@ -1,9 +1,74 @@
 package day13
 
+import (
+	"github.com/crab-apple/AoC2021/internal/input"
+	"github.com/crab-apple/AoC2021/internal/utils"
+	"github.com/crab-apple/AoC2021/internal/utils/grid"
+	"strings"
+)
+
 func SolvePart1(s string) int {
-	return 0
+	points, folds := parseInput(s)
+
+	points = applyFold(points, folds[0])
+
+	return len(points)
 }
 
 func SolvePart2(s string) int {
 	return 0
+}
+
+func applyFold(points utils.Set[grid.GridPosition], fold Fold) utils.Set[grid.GridPosition] {
+	r := utils.Set[grid.GridPosition]{}
+	for _, point := range utils.Keys(points) {
+		r.Add(ApplyFold(point, fold))
+	}
+	return r
+}
+
+func ApplyFold(point grid.GridPosition, fold Fold) grid.GridPosition {
+	x := point.Col
+	y := point.Row
+	if fold.Axis == "x" {
+		if x >= fold.Value {
+			x = fold.Value*2 - x
+		}
+	}
+	if fold.Axis == "y" {
+		if y >= fold.Value {
+			y = fold.Value*2 - y
+		}
+	}
+	return grid.GridPosition{x, y}
+}
+
+func parseInput(s string) (utils.Set[grid.GridPosition], []Fold) {
+	points := utils.Set[grid.GridPosition]{}
+	folds := make([]Fold, 0)
+	lines := input.ReadLines(s)
+	for _, line := range lines {
+		if utils.IsEmpty(line) {
+			continue
+		}
+		if strings.HasPrefix(line, "fold") {
+			fold := Fold{
+				strings.Split(strings.Split(line, " ")[2], "=")[0],
+				input.ToInt(strings.Split(strings.Split(line, " ")[2], "=")[1]),
+			}
+			folds = append(folds, fold)
+		} else {
+			point := grid.GridPosition{
+				input.ToInt(strings.Split(line, ",")[0]),
+				input.ToInt(strings.Split(line, ",")[1]),
+			}
+			points.Add(point)
+		}
+	}
+	return points, folds
+}
+
+type Fold struct {
+	Axis  string
+	Value int
 }
--- cmd/day13/solution_test.go
+++ cmd/day13/solution_test.go
@@ -3,20 +3,41 @@ package day13_test
 import (
 	"github.com/crab-apple/AoC2021/cmd/day13"
 	"github.com/crab-apple/AoC2021/internal/input"
+	"github.com/crab-apple/AoC2021/internal/utils/grid"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 var testInput = `
-Test input here
+6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5
 `
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, day13.SolvePart1(testInput))
+	assert.Equal(t, 17, day13.SolvePart1(testInput))
 }
 
 func TestPart1RealInput(t *testing.T) {
-	assert.Equal(t, 0, day13.SolvePart1(input.ReadInputFile()))
+	assert.Equal(t, 655, day13.SolvePart1(input.ReadInputFile()))
 }
 
 func TestPart2(t *testing.T) {
@@ -25,4 +46,11 @@ func TestPart2(t *testing.T) {
 
 func TestPart2RealInput(t *testing.T) {
 	assert.Equal(t, 0, day13.SolvePart2(input.ReadInputFile()))
+}
+
+func TestApplyFold(t *testing.T) {
+	p := grid.GridPosition{3, 4}
+	assert.Equal(t, grid.GridPosition{3, 4}, day13.ApplyFold(p, day13.Fold{"y", 5}))
+	assert.Equal(t, grid.GridPosition{3, 2}, day13.ApplyFold(p, day13.Fold{"y", 3}))
+	assert.Equal(t, grid.GridPosition{3, 0}, day13.ApplyFold(p, day13.Fold{"y", 2}))
 }
