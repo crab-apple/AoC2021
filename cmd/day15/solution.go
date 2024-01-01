1286193568517bd9abd63a6731448efa716232b0
crab-apple <andreu.bastardas@gmail.com>
2024-01-01 20:51:53 +0100

    Day 15 part 1

--- cmd/day15/solution.go
+++ cmd/day15/solution.go
@@ -1,7 +1,40 @@
 package day15
 
+import (
+	"github.com/crab-apple/AoC2021/internal/input"
+	"github.com/crab-apple/AoC2021/internal/utils"
+	"github.com/crab-apple/AoC2021/internal/utils/grid"
+)
+
 func SolvePart1(s string) int {
-	return 0
+
+	g := grid.NewGrid(s, func(r rune) int {
+		return input.ToInt(string(r))
+	})
+
+	costs := make(map[grid.GridPosition]int)
+	pending := utils.Queue[grid.GridPosition]{}
+
+	startingPosition := grid.GridPosition{0, 0}
+	costs[startingPosition] = 0
+	pending.Add(startingPosition)
+
+	for pending.Len() != 0 {
+		current := pending.RemoveFirst()
+		for _, n := range current.FourNeighbours() {
+			if g.HasPosition(n) {
+				cost := costs[current] + g.Get(n)
+				oldCost, ok := costs[n]
+				if !ok || oldCost > cost {
+					costs[n] = cost
+					pending.Add(n)
+				}
+			}
+		}
+	}
+
+	return costs[grid.GridPosition{g.NumCols() - 1, g.NumRows() - 1}]
+
 }
 
 func SolvePart2(s string) int {
--- cmd/day15/solution_test.go
+++ cmd/day15/solution_test.go
@@ -8,15 +8,24 @@ import (
 )
 
 var testInput = `
-Test input here
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
 `
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, day15.SolvePart1(testInput))
+	assert.Equal(t, 40, day15.SolvePart1(testInput))
 }
 
 func TestPart1RealInput(t *testing.T) {
-	assert.Equal(t, 0, day15.SolvePart1(input.ReadInputFile()))
+	assert.Equal(t, 363, day15.SolvePart1(input.ReadInputFile()))
 }
 
 func TestPart2(t *testing.T) {
