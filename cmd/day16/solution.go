79a2b9b85bc580db0c7b8c065843552f626ce8be
crab-apple <andreu.bastardas@gmail.com>
2024-01-02 11:23:44 +0100

    Day 16 part 1

--- cmd/day16/solution.go
+++ cmd/day16/solution.go
@@ -1,9 +1,190 @@
 package day16
 
+import (
+	"encoding/hex"
+	"fmt"
+)
+
 func SolvePart1(s string) int {
-	return 0
+	packet := NewPacket(s)
+	//printInfo(packet, 0)
+	return sumVersions(packet)
+}
+
+func printInfo(packet Packet, indent int) {
+
+	printIndented := func(s string) {
+		for i := 0; i < indent; i++ {
+			fmt.Print("  ")
+		}
+		fmt.Println(s)
+	}
+	printIndented("------------")
+	printIndented(fmt.Sprintf("Version: %v", packet.Version()))
+	printIndented(fmt.Sprintf("Type: %v", packet.TypeId()))
+	if packet.TypeId() == 4 {
+		printIndented(fmt.Sprintf("Value: %v", packet.LiteralValue()))
+	}
+	for _, sp := range packet.SubPackets() {
+		printInfo(sp, indent+1)
+	}
 }
 
 func SolvePart2(s string) int {
 	return 0
+}
+
+func sumVersions(p Packet) int {
+	r := p.Version()
+	for _, sp := range p.SubPackets() {
+		r += sumVersions(sp)
+	}
+	return r
+}
+
+type Packet struct {
+	bytes     []byte
+	bitOffset int
+}
+
+func NewPacket(s string) Packet {
+	bytes, err := hex.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	return Packet{bytes, 0}
+}
+
+func (p *Packet) Version() int {
+	return int(p.GetBits(0, 3))
+}
+
+func (p *Packet) TypeId() int {
+	return int(p.GetBits(3, 3))
+}
+
+func (p *Packet) GetBits(offset, len int) byte {
+	if len > 8 {
+		panic("unsupported")
+	}
+
+	effectiveOffset := p.bitOffset + offset
+	leftLen := min(len, 8-effectiveOffset%8)
+	rightLen := len - leftLen
+	left := p.getByte(effectiveOffset/8, effectiveOffset%8, leftLen)
+	right := byte(0)
+	if rightLen > 0 {
+		right = p.getByte(effectiveOffset/8+1, 0, rightLen)
+	}
+	return left<<rightLen + right
+}
+
+func (p *Packet) GetInt(offset, len int) int {
+	if len > 64 {
+		panic("unsupported")
+	}
+
+	if len <= 8 {
+		return int(p.GetBits(offset, len))
+	}
+	return p.GetInt(offset, len-8)<<8 + p.GetInt(offset+len-8, 8)
+}
+
+func (p *Packet) getByte(numByte, offset, len int) byte {
+	return p.bytes[numByte] << offset >> (8 - len)
+}
+
+func (p *Packet) LiteralValue() int {
+
+	if p.TypeId() != 4 {
+		panic("Unsupported (not literal)")
+	}
+
+	bytes := p.literalValueBits()
+
+	result := 0
+	for i, b := range bytes {
+		result += +int(b) << ((len(bytes) - 1 - i) * 4)
+	}
+	return result
+}
+
+func (p *Packet) literalValueBits() []byte {
+
+	if p.TypeId() != 4 {
+		panic("Unsupported (not literal)")
+	}
+
+	bytes := make([]byte, 0)
+
+	start := 6
+	for {
+		chunk := p.GetBits(start, 5)
+		more := chunk >> 4
+		bytes = append(bytes, chunk&0xF)
+		if more == 0 {
+			break
+		}
+		start += 5
+	}
+
+	return bytes
+}
+
+func (p *Packet) Length() int {
+
+	length := 6
+
+	if p.TypeId() == 4 {
+		// Literal
+		length += len(p.literalValueBits()) * 5
+	} else {
+		length += 1
+		lengthTypeId := p.GetBits(6, 1)
+		if lengthTypeId == 0 {
+			length += 15
+		} else {
+			length += 11
+		}
+		for _, subpacket := range p.SubPackets() {
+			length += subpacket.Length()
+		}
+	}
+	return length
+}
+
+func (p *Packet) SubPackets() []Packet {
+
+	subpackets := make([]Packet, 0)
+
+	if p.TypeId() == 4 {
+		// Literal
+		return subpackets
+	}
+
+	lengthTypeId := p.GetBits(6, 1)
+	if lengthTypeId == 0 {
+		contentLength := p.GetInt(7, 15)
+		subpacketOffset := 6 + 1 + 15
+		for contentLength > 0 {
+			subpacket := Packet{bytes: p.bytes, bitOffset: p.bitOffset + subpacketOffset}
+			subpacketOffset += subpacket.Length()
+			subpackets = append(subpackets, subpacket)
+			contentLength -= subpacket.Length()
+		}
+
+	} else {
+		numSubPackets := p.GetInt(7, 11)
+		subPacketOffset := 6 + 1 + 11
+		for {
+			subpacket := Packet{bytes: p.bytes, bitOffset: p.bitOffset + subPacketOffset}
+			subpackets = append(subpackets, subpacket)
+			numSubPackets--
+			if numSubPackets == 0 {
+				break
+			}
+			subPacketOffset += subpacket.Length()
+		}
+	}
+	return subpackets
 }
--- cmd/day16/solution_test.go
+++ cmd/day16/solution_test.go
@@ -1,28 +1,87 @@
 package day16_test
 
 import (
+	"fmt"
 	"github.com/crab-apple/AoC2021/cmd/day16"
 	"github.com/crab-apple/AoC2021/internal/input"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-var testInput = `
-Test input here
-`
-
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, day16.SolvePart1(testInput))
+
+	examples := []Part1TestCase{
+		{
+			"8A004A801A8002F478",
+			16,
+		},
+		{
+			"620080001611562C8802118E34",
+			12,
+		},
+		{
+			"C0015000016115A2E0802F182340",
+			23,
+		},
+		{
+			"A0016C880162017C3686B18A3D4780",
+			31,
+		},
+	}
+
+	for i, testCase := range examples {
+		t.Run(fmt.Sprintf("Example %v", i), func(t *testing.T) {
+			assert.Equal(t, testCase.wantOutput, day16.SolvePart1(testCase.input))
+		})
+	}
 }
 
 func TestPart1RealInput(t *testing.T) {
-	assert.Equal(t, 0, day16.SolvePart1(input.ReadInputFile()))
+	assert.Equal(t, 904, day16.SolvePart1(input.ReadInputFile()))
 }
 
 func TestPart2(t *testing.T) {
-	assert.Equal(t, 0, day16.SolvePart2(testInput))
+	assert.Equal(t, 0, day16.SolvePart2(""))
 }
 
 func TestPart2RealInput(t *testing.T) {
 	assert.Equal(t, 0, day16.SolvePart2(input.ReadInputFile()))
+}
+
+type Part1TestCase struct {
+	input      string
+	wantOutput int
+}
+
+func TestPacketVersion(t *testing.T) {
+	packet := day16.NewPacket("D2FE28")
+	assert.Equal(t, 6, packet.Version())
+}
+
+func TestTypeId(t *testing.T) {
+	packet := day16.NewPacket("D2FE28")
+	assert.Equal(t, 4, packet.TypeId())
+}
+
+func TestLiteralValue(t *testing.T) {
+	packet := day16.NewPacket("D2FE28")
+	assert.Equal(t, 2021, packet.LiteralValue())
+}
+
+func TestLength(t *testing.T) {
+	t.Run("Literal value", func(t *testing.T) {
+		packet := day16.NewPacket("D2FE28")
+		assert.Equal(t, 21, packet.Length())
+	})
+
+	t.Run("Operator length type 0", func(t *testing.T) {
+		packet := day16.NewPacket("38006F45291200")
+		assert.Equal(t, 49, packet.Length())
+	})
+
+	t.Run("Operator length type 1", func(t *testing.T) {
+		packet := day16.NewPacket("EE00D40C823060")
+		assert.Equal(t, 51, packet.Length())
+	})
+
 }
