7b5064ce32ce593dde565458a6369db623229564
crab-apple <andreu.bastardas@gmail.com>
2024-01-02 17:36:11 +0100

    Day 18 part 1

--- cmd/day18/solution.go
+++ cmd/day18/solution.go
@@ -1,9 +1,210 @@
 package day18
 
+import (
+	"bytes"
+	"fmt"
+	"github.com/crab-apple/AoC2021/internal/input"
+	"strconv"
+	"strings"
+)
+
 func SolvePart1(s string) int {
-	return 0
+	numbers := input.Read(s, func(s string) SnailNumber {
+		return ParseSnailNumber(s)
+	})
+	current := numbers[0]
+	for i := 1; i < len(numbers); i++ {
+		current = &PairNumber{current, numbers[i]}
+		Reduce(current)
+	}
+	return current.Magnitude()
 }
 
 func SolvePart2(s string) int {
 	return 0
+}
+
+func Reduce(number SnailNumber) {
+	for {
+		ok := number.Explode()
+		if ok {
+			continue
+		}
+		ok, _ = number.Split()
+		if !ok {
+			break
+		}
+	}
+}
+
+type SnailNumber interface {
+	String() string
+	Explode() bool
+	ExplodeLevels(levels int) (int, int, bool)
+	Split() (bool, SnailNumber)
+	SingleValue() int
+	AddToRightMost(int) bool
+	AddToLeftMost(int) bool
+	Magnitude() int
+}
+
+type PairNumber struct {
+	left, right SnailNumber
+}
+
+func (n *PairNumber) String() string {
+	return fmt.Sprintf("[%v,%v]", n.left, n.right)
+}
+
+func (n *PairNumber) Explode() bool {
+	_, _, ok := n.ExplodeLevels(4)
+	return ok
+}
+
+func (n *PairNumber) ExplodeLevels(levels int) (int, int, bool) {
+
+	if levels == 0 {
+		leftVal := n.left.SingleValue()
+		rightVal := n.right.SingleValue()
+
+		return leftVal, rightVal, true
+	}
+
+	left, right, ok := n.left.ExplodeLevels(levels - 1)
+	if ok {
+		if levels == 1 {
+			n.left = &SingleNumber{0}
+		}
+		if right != 0 && n.right.AddToLeftMost(right) {
+			right = 0
+		}
+		return left, right, true
+	}
+	left, right, ok = n.right.ExplodeLevels(levels - 1)
+	if ok {
+		if levels == 1 {
+			n.right = &SingleNumber{0}
+		}
+		if left != 0 && n.left.AddToRightMost(left) {
+			left = 0
+		}
+		return left, right, true
+	}
+
+	return 0, 0, false
+}
+
+func (n *PairNumber) Split() (bool, SnailNumber) {
+	ok, replacement := n.left.Split()
+	if ok {
+		if replacement != nil {
+			n.left = replacement
+		}
+		return true, nil
+	}
+	ok, replacement = n.right.Split()
+	if ok {
+		if replacement != nil {
+			n.right = replacement
+		}
+		return true, nil
+	}
+	return false, nil
+}
+
+func (n *PairNumber) AddToRightMost(val int) bool {
+
+	ok := n.right.AddToRightMost(val)
+	if !ok {
+		ok = n.left.AddToRightMost(val)
+	}
+	return ok
+}
+
+func (n *PairNumber) AddToLeftMost(val int) bool {
+
+	ok := n.left.AddToLeftMost(val)
+	if !ok {
+		ok = n.right.AddToLeftMost(val)
+	}
+	return ok
+}
+
+func (n *PairNumber) SingleValue() int {
+	panic("Not a single number")
+}
+
+func (n *PairNumber) Magnitude() int {
+	return 3*n.left.Magnitude() + 2*n.right.Magnitude()
+}
+
+type SingleNumber struct {
+	val int
+}
+
+func (n *SingleNumber) String() string {
+	return strconv.Itoa(n.val)
+}
+func (n *SingleNumber) Explode() bool {
+	return false
+}
+
+func (n *SingleNumber) ExplodeLevels(levels int) (int, int, bool) {
+	return 0, 0, false
+}
+
+func (n *SingleNumber) Split() (bool, SnailNumber) {
+	if n.val > 9 {
+		leftVal := n.val / 2
+		rightVal := n.val - leftVal
+		return true, &PairNumber{&SingleNumber{leftVal}, &SingleNumber{rightVal}}
+	}
+	return false, nil
+}
+
+func (n *SingleNumber) SingleValue() int {
+	return n.val
+}
+
+func (n *SingleNumber) AddToRightMost(val int) bool {
+	n.val += val
+	return true
+}
+
+func (n *SingleNumber) AddToLeftMost(val int) bool {
+	n.val += val
+	return true
+}
+
+func (n *SingleNumber) Magnitude() int {
+	return n.val
+}
+
+func ParseSnailNumber(str string) SnailNumber {
+
+	if strings.HasPrefix(str, "[") {
+		str = strings.TrimPrefix(str, "[")
+		str = strings.TrimSuffix(str, "]")
+		// Now find where the split is
+		runes := bytes.Runes([]byte(str))
+		splitPosition := 0
+		bracketCount := 0
+		for i, r := range runes {
+			if r == ',' && bracketCount == 0 {
+				splitPosition = i
+				break
+			}
+			if r == '[' {
+				bracketCount++
+			}
+			if r == ']' {
+				bracketCount--
+			}
+		}
+		leftStr := string(runes[:splitPosition])
+		rightStr := string(runes[splitPosition+1:])
+		return &PairNumber{ParseSnailNumber(leftStr), ParseSnailNumber(rightStr)}
+	}
+
+	return &SingleNumber{input.ToInt(str)}
 }
--- cmd/day18/solution_test.go
+++ cmd/day18/solution_test.go
@@ -1,6 +1,7 @@
 package day18_test
 
 import (
+	"fmt"
 	"github.com/crab-apple/AoC2021/cmd/day18"
 	"github.com/crab-apple/AoC2021/internal/input"
 	"github.com/stretchr/testify/assert"
@@ -8,15 +9,24 @@ import (
 )
 
 var testInput = `
-Test input here
+[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
 `
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, day18.SolvePart1(testInput))
+	assert.Equal(t, 4140, day18.SolvePart1(testInput))
 }
 
 func TestPart1RealInput(t *testing.T) {
-	assert.Equal(t, 0, day18.SolvePart1(input.ReadInputFile()))
+	assert.Equal(t, 4072, day18.SolvePart1(input.ReadInputFile()))
 }
 
 func TestPart2(t *testing.T) {
@@ -25,4 +35,82 @@ func TestPart2(t *testing.T) {
 
 func TestPart2RealInput(t *testing.T) {
 	assert.Equal(t, 0, day18.SolvePart2(input.ReadInputFile()))
+}
+
+func TestParse(t *testing.T) {
+	str := "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]"
+	snailNumber := day18.ParseSnailNumber(str)
+	assert.Equal(t, str, snailNumber.String())
+}
+
+func TestExplode(t *testing.T) {
+	cases := []TestCase{
+		{"[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]"},
+		{"[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]"},
+		{"[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]"},
+		{"[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"},
+		{"[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"},
+	}
+
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("%v should become %v after explosion", c.input, c.output), func(t *testing.T) {
+			snailNumber := day18.ParseSnailNumber(c.input)
+			snailNumber.Explode()
+			assert.Equal(t, c.output, snailNumber.String())
+		})
+	}
+}
+
+func TestSplit(t *testing.T) {
+	cases := []TestCase{
+		{"[[[[0,7],4],[15,[0,13]]],[1,1]]", "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"},
+		{"[[[[0,7],4],[[7,8],[0,13]]],[1,1]]", "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]"},
+	}
+
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("%v should become %v after split", c.input, c.output), func(t *testing.T) {
+			snailNumber := day18.ParseSnailNumber(c.input)
+			snailNumber.Split()
+			assert.Equal(t, c.output, snailNumber.String())
+		})
+	}
+}
+
+func TestReduce(t *testing.T) {
+	cases := []TestCase{
+		{"[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]", "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"},
+	}
+
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("%v should become %v after reduction", c.input, c.output), func(t *testing.T) {
+			snailNumber := day18.ParseSnailNumber(c.input)
+			day18.Reduce(snailNumber)
+			assert.Equal(t, c.output, snailNumber.String())
+		})
+	}
+}
+
+func TestMagnitude(t *testing.T) {
+	cases := []struct {
+		input  string
+		output int
+	}{
+		{"[[1,2],[[3,4],5]]", 143},
+		{"[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", 1384},
+		{"[[[[1,1],[2,2]],[3,3]],[4,4]]", 445},
+		{"[[[[3,0],[5,3]],[4,4]],[5,5]]", 791},
+		{"[[[[5,0],[7,4]],[5,5]],[6,6]]", 1137},
+		{"[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", 3488},
+	}
+
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("%v should become %v after reduction", c.input, c.output), func(t *testing.T) {
+			snailNumber := day18.ParseSnailNumber(c.input)
+			assert.Equal(t, c.output, snailNumber.Magnitude())
+		})
+	}
+}
+
+type TestCase struct {
+	input, output string
 }
